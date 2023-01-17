fe74e98df1145a9d6f0b5f2f02bdf10ed6ea712f
clevessonmendonca <wanderbloks311@gmail.com>
2023-01-17 20:07:00 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit />
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Habit = () => {
+  return (
+    <div className="bg-zinc-900">Habit</div>
+  )
+}
